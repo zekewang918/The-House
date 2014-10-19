0bc7cf03b3ff71d2d1fcdcf1cac74221ea460599
Xike Wang <zekewang918@gmail.com>
2014-10-19 04:47:01 -0300

    Main Menu: 1:Create Own GUIStyle 2:Delte Border and Padding 3:Add Link to Armoire and Team

--- The House/Assets/Scripts/MainMenuGUI.cs
+++ The House/Assets/Scripts/MainMenuGUI.cs
@@ -2,19 +2,27 @@
 using System.Collections;
 
 public class MainMenuGUI : MonoBehaviour {
-	
-	private float newW;
-	private float newH;
 
 	public Texture logo;
+	public Texture start;
+	public Texture team;
+	public GUIStyle myStyle;
 
 	void OnGUI(){
-		GUI.DrawTexture(new Rect(0,0,newW,newH), logo, ScaleMode.ScaleToFit, true, 10.0f);
-		
-	}
-	void Awake(){
-		newW = Screen.width / 2;
-		newH = Screen.height / 2;
+		GUILayout.BeginArea (new Rect (0, 0, Screen.width, Screen.height), myStyle);
+		GUI.DrawTexture(new Rect(0,0,Screen.width/2,Screen.height/2), logo, ScaleMode.ScaleToFit, true, 10.0f);
+		//if (GUI.DrawTexture (new Rect (Screen.width * 0.7f, Screen.height * 0.6f, Screen.width * 0.2f, Screen.height * 0.2f), start, ScaleMode.ScaleToFit, true, 10.0f)) {
+		//	Application.LoadLevel("Armoire");
+		//}
+		//GUI.DrawTexture(new Rect(Screen.width * 0.7f, Screen.height * 0.7f, Screen.width * 0.15f, Screen.height * 0.2f), team, ScaleMode.ScaleToFit, true, 10.0f);
 
+		if (GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.6f, Screen.width * 0.2f, Screen.height * 0.2f), start, myStyle)){
+			Application.LoadLevel("Armoire");
+		}
+		if (GUI.Button(new Rect(Screen.width * 0.72f, Screen.height * 0.7f, Screen.width * 0.15f, Screen.height * 0.2f), team, myStyle)){
+			Application.LoadLevel("Team");
+		}
+		GUILayout.EndArea ();
 	}
+
 }
