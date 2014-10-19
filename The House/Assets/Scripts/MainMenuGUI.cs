19fc3ee71177b58c9b72f4d6f8419089448652f5
Xike Wang <zekewang918@gmail.com>
2014-10-19 03:16:21 -0300

    Main Menu: Add Title

---
+++ The House/Assets/Scripts/MainMenuGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuGUI : MonoBehaviour {
+	
+	private float newW;
+	private float newH;
+
+	public Texture logo;
+
+	void OnGUI(){
+		GUI.DrawTexture(new Rect(0,0,newW,newH), logo, ScaleMode.ScaleToFit, true, 10.0f);
+		
+	}
+	void Awake(){
+		newW = Screen.width / 2;
+		newH = Screen.height / 2;
+
+	}
+}
