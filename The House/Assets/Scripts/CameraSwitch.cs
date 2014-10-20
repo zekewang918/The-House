667e77090da84f370e461b5d2ea48c8b16fcf7f0
Xike Wang <zekewang918@gmail.com>
2014-10-20 01:11:30 -0300

    Armoire: 1: Add Carema for Locker Puzzle 2: Add CameraSwtich.cs script

---
+++ The House/Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera player;
+	public Camera armoire_locker;
+
+	void cameraSwitch(int n){
+		if (n == 0){
+			player.enabled = true;
+			armoire_locker.enabled = false;
+		}else if (n == 1){
+			player.enabled = false;
+			armoire_locker.enabled = true;
+		}
+	}
+}
