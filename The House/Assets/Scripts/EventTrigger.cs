01a68fa0b1a22ce909aa2595371a3f3d5902ca20
Xike Wang <zekewang918@gmail.com>
2014-10-20 13:18:45 -0300

    Weird Commit...Just Ignore It

--- The House/Assets/Scripts/EventTrigger.cs
+++ The House/Assets/Scripts/EventTrigger.cs
@@ -3,23 +3,20 @@ using System.Collections;
 
 public class EventTrigger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		TouchEvent ();
+	public float rayLength;
+	public GameObject player;
+
+
+	void Start(){
 	}
 
 	void TouchEvent(){
-		// Create a ray named hitEnemy from its position and aim down
-		Ray eventToucher = new Ray (this.transform.position, Vector3.forward);
+		// Create a ray named eventToucher from its position and aim forward
+		Ray eventToucher = new Ray (player.transform.position, Vector3.forward);
 		// Create a raycast called hit
 		RaycastHit hit;
-		Debug.DrawRay (this.transform.position, Vector3.forward);
-		// If the Raycast hit a collider that tag is enyme then destory it.
+		Debug.DrawRay (player.transform.position, Vector3.forward);
+		// If the Raycast hit a collider that tag is event
 		if (Physics.Raycast(eventToucher, out hit, 0.3f)){
 			// Check if the collider that hits is Enemy
 			
