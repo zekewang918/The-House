e112a68c2d69723e1509de0571e247e98bf3493f
Xike Wang <zekewang918@gmail.com>
2014-10-30 02:03:12 -0300

    Armoire: Automatically print text one by one

--- The House/Assets/Scripts/GameGUI.cs
+++ The House/Assets/Scripts/GameGUI.cs
@@ -13,6 +13,8 @@ public class GameGUI : MonoBehaviour {
 
 	private string temp = "";
 
+	private int index = 0;
+
 	void OnGUI(){
 		for (int i = 0; i < 6; i++){
 			GUI.DrawTexture(new Rect(Screen.width * 0.2f + i * Screen.width * 0.1f,Screen.height * 0.9f,50.0f,50.0f), items[i]);
@@ -20,10 +22,16 @@ public class GameGUI : MonoBehaviour {
 
 		Rect drect = new Rect (Screen.width * 0.15f, Screen.height*0.7f, Screen.width*0.85f, Screen.height*0.3f);
 		GUI.DrawTexture (drect, dialogBG);
-		for (int i = 0; i < speech.Length; i++){
-			temp += speech[i];
+		if (notEnd()){
 			GUI.Label (new Rect (0, 0, 100, 100), temp);
 		}
+	}
+	bool notEnd(){
+		if (index < speech.Length){
+			temp += speech[index];
+			index++;
+		}
+		return true;
 	}
 
 }
