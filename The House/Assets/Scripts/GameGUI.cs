a8a9a8c737d3c3868473fde3ec35b4f5ad039dfe
Xike Wang <zekewang918@gmail.com>
2014-10-30 01:48:14 -0300

    Add Dialog and text

--- The House/Assets/Scripts/GameGUI.cs
+++ The House/Assets/Scripts/GameGUI.cs
@@ -5,9 +5,25 @@ public class GameGUI : MonoBehaviour {
 
 	public Texture[] items = new Texture[6];
 
+	public string speech = "Ahhhh..... TEST TEST!";
+
+	public Texture dialogBG;
+
+	public GUIText mtext;
+
+	private string temp = "";
+
 	void OnGUI(){
 		for (int i = 0; i < 6; i++){
 			GUI.DrawTexture(new Rect(Screen.width * 0.2f + i * Screen.width * 0.1f,Screen.height * 0.9f,50.0f,50.0f), items[i]);
 		}
+
+		Rect drect = new Rect (Screen.width * 0.15f, Screen.height*0.7f, Screen.width*0.85f, Screen.height*0.3f);
+		GUI.DrawTexture (drect, dialogBG);
+		for (int i = 0; i < speech.Length; i++){
+			temp += speech[i];
+			GUI.Label (new Rect (0, 0, 100, 100), temp);
+		}
 	}
+
 }
